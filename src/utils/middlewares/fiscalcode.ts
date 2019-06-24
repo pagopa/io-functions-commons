04cfbaf167655a5963e224cd3565b283ad51d679
Federico Feroldi <cloudify@users.noreply.github.com>
2019-06-24 17:26:36 +0200

    Imports middlewares from digital-citizenship-functions (#4)

---
+++ src/utils/middlewares/fiscalcode.ts
@@ -0,0 +1,13 @@
+import { FiscalCode } from "../../../generated/definitions/FiscalCode";
+
+import { RequiredParamMiddleware } from "./required_param";
+
+/**
+ * A request middleware that validates the presence of a valid `fiscalcode` parameter
+ * in the request. In case the parameter is missing or is not valid, the middleware
+ * returns an `IResponseErrorValidation`.
+ */
+export const FiscalCodeMiddleware = RequiredParamMiddleware(
+  "fiscalcode",
+  FiscalCode
+);
