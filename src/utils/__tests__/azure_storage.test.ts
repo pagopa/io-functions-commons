66aa40e2e31c9b003f0073144b7d93ef72d55805
Francesco Persico <francescopersico@gmail.com>
2019-09-16 14:41:46 +0200

    Return None on BlobNotFound (#22)

---
+++ src/utils/__tests__/azure_storage.test.ts
@@ -0,0 +1,59 @@
+/* tslint:disable: no-any */
+
+import * as t from "io-ts";
+
+import { isRight } from "fp-ts/lib/Either";
+import { isNone } from "fp-ts/lib/Option";
+
+import {
+  BlobNotFoundCode,
+  getBlobAsObject,
+  getBlobAsText
+} from "../azure_storage";
+
+const TestObject = t.interface({
+  prop: t.string
+});
+
+type TestObject = t.TypeOf<typeof TestObject>;
+
+const blobServiceMock = {
+  getBlobToText: jest.fn((_, __, f) => {
+    f({
+      code: BlobNotFoundCode
+    });
+  })
+};
+
+describe("getBlobAsText", () => {
+  it("should return None on BlobNotFound error", async () => {
+    const errorOrMaybeText = await getBlobAsText(
+      blobServiceMock as any,
+      undefined as any,
+      undefined as any
+    );
+
+    expect(isRight(errorOrMaybeText)).toBe(true);
+    if (isRight(errorOrMaybeText)) {
+      const maybeText = errorOrMaybeText.value;
+      expect(isNone(maybeText)).toBe(true);
+    }
+  });
+});
+
+describe("getBlobAsObject", () => {
+  it("should return None on BlobNotFound error", async () => {
+    const errorOrMaybeObject = await getBlobAsObject(
+      TestObject,
+      blobServiceMock as any,
+      undefined as any,
+      undefined as any
+    );
+
+    expect(isRight(errorOrMaybeObject)).toBe(true);
+    if (isRight(errorOrMaybeObject)) {
+      const maybeObject = errorOrMaybeObject.value;
+      expect(isNone(maybeObject)).toEqual(true);
+    }
+  });
+});
