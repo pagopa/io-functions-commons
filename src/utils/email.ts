076874cfc1c1002e8e70720ce3a9d6647617c973
Francesco Persico <francescopersico@gmail.com>
2019-11-06 09:47:28 +0100

    [#169589413] Email related functions and templates (#35)

---
+++ src/utils/email.ts
@@ -0,0 +1,19 @@
+import { Either, left, right } from "fp-ts/lib/Either";
+import * as NodeMailer from "nodemailer";
+
+/**
+ * Promise wrapper around Transporter#sendMail
+ */
+export async function sendMail(
+  transporter: NodeMailer.Transporter,
+  options: NodeMailer.SendMailOptions
+): Promise<Either<Error, NodeMailer.SentMessageInfo>> {
+  return new Promise<Either<Error, NodeMailer.SentMessageInfo>>(resolve => {
+    transporter.sendMail(options, (err, res) => {
+      const result: Either<Error, NodeMailer.SentMessageInfo> = err
+        ? left(err)
+        : right(res);
+      resolve(result);
+    });
+  });
+}
