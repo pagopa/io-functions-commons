64e597cd7926b4f939f409bd438a46e2aca6bc06
Danilo Spinelli <gunzip@users.noreply.github.com>
2020-03-09 11:43:50 +0100

    [#171684904] add requireSecureChannels to visible services (#53)

--- src/models/visible_service.ts
+++ src/models/visible_service.ts
@@ -5,26 +5,40 @@
 import * as t from "io-ts";
 
 import { collect, StrMap } from "fp-ts/lib/StrMap";
-import { NonEmptyString } from "italia-ts-commons/lib/strings";
-import { OrganizationFiscalCode } from "../../generated/definitions/OrganizationFiscalCode";
-import { ServiceId } from "../../generated/definitions/ServiceId";
+import { pick } from "italia-ts-commons/lib/types";
+import {
+  NotificationChannel,
+  NotificationChannelEnum
+} from "../../generated/definitions/NotificationChannel";
 import { ServicePublic } from "../../generated/definitions/ServicePublic";
+import { ServiceScopeEnum } from "../../generated/definitions/ServiceScope";
 import { ServiceTuple } from "../../generated/definitions/ServiceTuple";
-import { ServiceMetadata } from "./service";
+import * as DocumentDbUtils from "../utils/documentdb";
+import { VersionedModel } from "../utils/documentdb_model_versioned";
+import { Service, ServiceMetadata } from "./service";
 
-// this is not a CosmosDB model, but entities are stored into blob storage
+// This is not a CosmosDB model, but entities are stored into blob storage
 export const VISIBLE_SERVICE_CONTAINER = "cached";
 export const VISIBLE_SERVICE_BLOB_ID = "visible-services.json";
 
-const VisibleServiceR = t.type({
-  departmentName: NonEmptyString,
-  id: NonEmptyString,
-  organizationFiscalCode: OrganizationFiscalCode,
-  organizationName: NonEmptyString,
-  serviceId: ServiceId,
-  serviceName: NonEmptyString,
-  version: t.Integer
-});
+// Public view of RetrivedService type
+const VisibleServiceR = t.intersection([
+  DocumentDbUtils.NewDocument,
+  VersionedModel,
+  t.type({
+    ...pick(
+      [
+        "departmentName",
+        "organizationFiscalCode",
+        "organizationName",
+        "requireSecureChannels",
+        "serviceId",
+        "serviceName"
+      ],
+      Service.types[0].props
+    )
+  })
+]);
 
 const VisibleServiceO = t.partial({
   serviceMetadata: ServiceMetadata
@@ -37,13 +51,39 @@ export const VisibleService = t.intersection(
 
 export type VisibleService = t.TypeOf<typeof VisibleService>;
 
+export function serviceAvailableNotificationChannels(
+  visibleService: VisibleService
+): ReadonlyArray<NotificationChannel> {
+  if (visibleService.requireSecureChannels) {
+    return [NotificationChannelEnum.WEBHOOK];
+  }
+  return [NotificationChannelEnum.EMAIL, NotificationChannelEnum.WEBHOOK];
+}
+
 export function toServicePublic(visibleService: VisibleService): ServicePublic {
   return {
+    available_notification_channels: serviceAvailableNotificationChannels(
+      visibleService
+    ),
     department_name: visibleService.departmentName,
     organization_fiscal_code: visibleService.organizationFiscalCode,
     organization_name: visibleService.organizationName,
     service_id: visibleService.serviceId,
-    service_metadata: visibleService.serviceMetadata,
+    service_metadata: visibleService.serviceMetadata
+      ? {
+          address: visibleService.serviceMetadata.address,
+          app_android: visibleService.serviceMetadata.appAndroid,
+          app_ios: visibleService.serviceMetadata.appIos,
+          description: visibleService.serviceMetadata.description,
+          email: visibleService.serviceMetadata.email,
+          pec: visibleService.serviceMetadata.pec,
+          phone: visibleService.serviceMetadata.phone,
+          privacy_url: visibleService.serviceMetadata.privacyUrl,
+          scope: visibleService.serviceMetadata.scope,
+          tos_url: visibleService.serviceMetadata.tosUrl,
+          web_url: visibleService.serviceMetadata.webUrl
+        }
+      : undefined,
     service_name: visibleService.serviceName,
     version: visibleService.version
   };
@@ -59,6 +99,10 @@ export function toServicesTuple(
   visibleServices: StrMap<VisibleService>
 ): ReadonlyArray<ServiceTuple> {
   return collect(visibleServices, (_, v) => ({
+    scope:
+      v.serviceMetadata && v.serviceMetadata.scope === ServiceScopeEnum.LOCAL
+        ? ServiceScopeEnum.LOCAL
+        : ServiceScopeEnum.NATIONAL,
     service_id: v.serviceId,
     version: v.version
   }));
