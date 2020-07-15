cfd5b447f361feb2695c9084a77c8bc47f12611b
Federico Feroldi <federico@teamdigitale.governo.it>
2020-07-15 21:06:47 +0200

    fixes to response.ts

--- src/utils/async.ts
+++ src/utils/async.ts
@@ -1,4 +1,20 @@
 /**
+ * Maps over an AsyncIterator
+ */
+export function mapAsyncIterator<T, V>(
+  iter: AsyncIterator<T>,
+  f: (t: T) => V
+): AsyncIterator<V> {
+  return {
+    next: () =>
+      iter.next().then(({ done, value }) => ({
+        done,
+        value: f(value)
+      }))
+  };
+}
+
+/**
  * Maps over an AsyncIterable
  */
 export function mapAsyncIterable<T, V>(
@@ -6,13 +22,29 @@ export function mapAsyncIterable<T, V>(
   f: (t: T) => V
 ): AsyncIterable<V> {
   const iter = source[Symbol.asyncIterator]();
+  const iterMapped = mapAsyncIterator(iter, f);
   return {
-    [Symbol.asyncIterator]: () => ({
-      next: () =>
-        iter.next().then(({ done, value }) => ({
-          done,
-          value: f(value)
-        }))
-    })
+    [Symbol.asyncIterator]: () => iterMapped
   };
+}
+
+export async function asyncIteratorToArray<T>(
+  iter: AsyncIterator<T>
+): Promise<ReadonlyArray<T>> {
+  const acc = Array<T>();
+
+  while (true) {
+    const next = await iter.next();
+    if (next.done === true) {
+      return acc;
+    }
+    acc.push(next.value);
+  }
+}
+
+export async function asyncIterableToArray<T>(
+  source: AsyncIterable<T>
+): Promise<ReadonlyArray<T>> {
+  const iter = source[Symbol.asyncIterator]();
+  return asyncIteratorToArray(iter);
 }
