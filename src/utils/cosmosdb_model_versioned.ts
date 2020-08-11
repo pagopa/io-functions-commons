9767e0031add3bbfbb2a20a7e756fd789f93299d
danilo spinelli <danilo.spinelli@pagopa.itt>
2020-08-11 14:42:25 +0200

    comment

--- src/utils/cosmosdb_model_versioned.ts
+++ src/utils/cosmosdb_model_versioned.ts
@@ -118,15 +118,18 @@ export abstract class CosmosdbModelVersioned<
   };
 
   /**
-   * Creates a new version from a full item definition. By a caller perspective, it should behave just like a normal upsert.
-   * If the item has a version defined, version is increased by one. Otherwise, the version is calculated from the latest item on db.
+   * Creates a new version from a full item definition.
+   *
+   * If the provided item has a version defined it is increased by one.
+   * Otherwise the version is computed retrieving the latest stored item revision.
+   *
    * When creating the new item, it performs an optimistic lock on the pair (modelId, version).
-   * If there is already an item with such pair (which is the case that the item has been update concurrently by another workflow), it returns a conflict error (code: 409)
+   * If there is already an item with such pair (which is the case that the item has been
+   * update concurrently by another workflow) it returns a conflict error (code: 409)
    *
    * @param o the item to be updated
    * @param requestOptions
    */
-
   public upsert = (
     o: TN,
     requestOptions?: RequestOptions
