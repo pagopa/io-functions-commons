0942a97cf80aaea69a50d75855904ebc467d18a5
gquadrati <greta.quadrati@pagopa.it>
2021-05-14 11:13:31 +0200

    wip

--- openapi/__tests__/definitions.test.ts
+++ openapi/__tests__/definitions.test.ts
@@ -1,6 +1,18 @@
+import { CreatedMessageWithContent } from "../../generated/definitions/CreatedMessageWithContent";
+import { CreatedMessageWithoutContent } from "../../generated/definitions/CreatedMessageWithoutContent";
 import { HiddenServicePayload } from "../../generated/definitions/HiddenServicePayload";
 import { ServicePayload } from "../../generated/definitions/ServicePayload";
 import { VisibleServicePayload } from "../../generated/definitions/VisibleServicePayload";
+
+import { toString } from "fp-ts/lib/function";
+import { FiscalCode } from "../../generated/definitions/FiscalCode";
+import { PaymentData } from "../../generated/definitions/PaymentData";
+import { OrganizationFiscalCode } from "../../generated/definitions/OrganizationFiscalCode";
+import { PaymentAmount } from "../../generated/definitions/PaymentAmount";
+import { PaymentNoticeNumber } from "../../generated/definitions/PaymentNoticeNumber";
+import { Payee } from "../../generated/definitions/Payee";
+import { PaymentDataWithPayee } from "../../generated/definitions/PaymentDataWithPayee";
+import { MessageContent } from "../../generated/definitions/MessageContent";
 
 describe("ServicePayload definition", () => {
   const commonServicePayload = {
@@ -107,5 +119,73 @@ describe("ServicePayload definition", () => {
     expect(servicePayloadTest.isRight()).toBe(true);
     expect(visibleServiceTest.isLeft()).toBe(true);
     expect(hiddenServiceTest.isRight()).toBe(true);
+  });
+});
+
+describe("NewMessage definition", () => {
+  const aFiscalCode = "FRLFRC74E04B157I" as FiscalCode;
+  const anOrganizationFiscalCode = "FRLFRC74E04B157I" as OrganizationFiscalCode;
+  const aDate = new Date();
+  const aMessageWithoutContent = {
+    id: "A_MESSAGE_ID",
+    fiscal_code: aFiscalCode,
+    created_at: aDate,
+    sender_service_id: "test"
+  };
+
+  const aCountentWithoutPaymentData = {
+    subject:
+      "A Subject of more than 80 characters. Try to reach this value with stupid words, but it's too hard!!! Very hard!!!!!!",
+    markdown:
+      "A markdown of more than 80 characters. Try to reach this value with stupid words, but it's too hard!!! Very hard!!!!!!!"
+  };
+
+  const aPaymentDataWithoutPayee: PaymentData = {
+    amount: 10.0 as PaymentAmount,
+    notice_number: "1001242" as PaymentNoticeNumber
+  };
+
+  const aPayee: Payee = { fiscal_code: anOrganizationFiscalCode };
+
+  it("should decode message without content", () => {
+    const messageWithoutContent = CreatedMessageWithoutContent.decode(
+      aMessageWithoutContent
+    );
+
+    expect(messageWithoutContent.isRight()).toBe(true);
+  });
+
+  it("should decode message with content but without payment data", () => {
+    const aMessageWithContent = {
+      ...aMessageWithoutContent,
+      content: aCountentWithoutPaymentData
+    };
+
+    const messageWithContent = CreatedMessageWithContent.decode(
+      aMessageWithContent
+    );
+
+    expect(messageWithContent.isRight()).toBe(true);
+  });
+
+  it("should not decode message with content and payment data but without payee", () => {
+    const aMessageWithContentWithPaymentDataWithoutPayee  = {
+      ...aMessageWithoutContent,
+      content: {
+        ...aCountentWithoutPaymentData,
+        payment_data: aPaymentDataWithoutPayee
+      }
+    };
+
+    const messageWithContent = MessageContent.decode(
+      {
+        ...aCountentWithoutPaymentData,
+        payment_data: aPaymentDataWithoutPayee
+      }
+    );
+
+    
+    
+    expect(messageWithContent.isLeft()).toBe(true);
   });
 });
