1a1ed180bf1f0debf96c30640d7dca4563f95683
fabriziopapi <fabrizio.papi@pagopa.it>
2021-08-02 15:07:39 +0200

    update messages.ts

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -3,12 +3,17 @@ import * as t from "io-ts";
 import winston = require("winston");
 
 import { toError } from "fp-ts/lib/Either";
-import { fromEither, none, Option, some } from "fp-ts/lib/Option";
+import { none, Option, some } from "fp-ts/lib/Option";
+import * as O from "fp-ts/lib/Option";
 
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 
-import { array } from "fp-ts/lib/Array";
-import { taskEither, TaskEither, tryCatch } from "fp-ts/lib/TaskEither";
+import * as A from "fp-ts/lib/Array";
+
+import { taskEither, TaskEither } from "fp-ts/lib/TaskEither";
+import * as TE from "fp-ts/lib/TaskEither";
+
+import { pipe } from "fp-ts/lib/function";
 import { RetrievedMessage } from "../models/message";
 import { NotificationModel } from "../models/notification";
 import { NotificationStatusModel } from "../models/notification_status";
@@ -34,13 +39,17 @@ export const getChannelStatus = async (
   notificationId: NonEmptyString,
   channel: NotificationChannelEnum
 ): Promise<NotificationChannelStatusValueEnum | undefined> => {
-  const errorOrMaybeStatus = await notificationStatusModel
-    .findOneNotificationStatusByNotificationChannel(notificationId, channel)
-    .run();
-  return fromEither(errorOrMaybeStatus)
-    .chain(t.identity)
-    .map(o => o.status)
-    .toUndefined();
+  const errorOrMaybeStatus = await notificationStatusModel.findOneNotificationStatusByNotificationChannel(
+    notificationId,
+    channel
+  )();
+
+  return pipe(
+    O.fromEither(errorOrMaybeStatus),
+    O.chain(t.identity),
+    O.map(o => o.status),
+    O.toUndefined
+  );
 };
 
 /**
@@ -58,58 +67,68 @@ export const getMessageNotificationStatuses = (
   notificationStatusModel: NotificationStatusModel,
   messageId: NonEmptyString
 ): TaskEither<Error, Option<NotificationStatusHolder>> =>
-  notificationModel
-    .findNotificationForMessage(messageId)
-    .mapLeft(error => {
+  pipe(
+    notificationModel.findNotificationForMessage(messageId),
+    TE.mapLeft(error => {
       // temporary log COSMOS_ERROR_RESPONSE kind due to body unavailability
       winston.error(`getMessageNotificationStatuses|Query error|${error.kind}`);
       return new Error(`Error querying for NotificationStatus`);
-    })
-    .chain(maybeNotification =>
+    }),
+    TE.chain(maybeNotification =>
       // It may happen that the notification object is not yet created in the database
       // due to some latency, so it's better to not fail here but return an empty object
-      maybeNotification.foldL(
-        () => {
-          winston.debug(
-            `getMessageNotificationStatuses|Notification not found|messageId=${messageId}`
-          );
-          return taskEither.of<Error, Option<NotificationStatusHolder>>(none);
-        },
-        notification =>
-          array
-            .sequence(taskEither)(
-              // collect the statuses of all channels
-              Object.keys(NotificationChannelEnum)
-                .map(k => NotificationChannelEnum[k as NotificationChannelEnum])
-                .map(channel =>
-                  tryCatch(
-                    () =>
-                      getChannelStatus(
-                        notificationStatusModel,
-                        // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-                        notification.id as NonEmptyString,
-                        channel
+      pipe(
+        maybeNotification,
+        O.fold(
+          () => {
+            winston.debug(
+              `getMessageNotificationStatuses|Notification not found|messageId=${messageId}`
+            );
+            return TE.of<Error, Option<NotificationStatusHolder>>(none);
+          },
+          notification =>
+            pipe(
+              A.sequence(taskEither)(
+                // collect the statuses of all channels
+                Object.keys(NotificationChannelEnum)
+                  .map(
+                    k => NotificationChannelEnum[k as NotificationChannelEnum]
+                  )
+                  .map(channel =>
+                    pipe(
+                      TE.tryCatch(
+                        () =>
+                          getChannelStatus(
+                            notificationStatusModel,
+                            // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+                            notification.id as NonEmptyString,
+                            channel
+                          ),
+                        toError
                       ),
-                    toError
-                  ).map(status => ({ channel, status }))
+                      TE.map(status => ({ channel, status }))
+                    )
+                  )
+              ),
+              TE.map(channelStatuses =>
+                // reduce the statuses in one response
+                channelStatuses.reduce<NotificationStatusHolder>(
+                  (a, s) =>
+                    s.status
+                      ? {
+                          ...a,
+                          [s.channel.toLowerCase()]: s.status
+                        }
+                      : a,
+                  {}
                 )
+              ),
+              TE.map(response => some(response))
             )
-            .map(channelStatuses =>
-              // reduce the statuses in one response
-              channelStatuses.reduce<NotificationStatusHolder>(
-                (a, s) =>
-                  s.status
-                    ? {
-                        ...a,
-                        [s.channel.toLowerCase()]: s.status
-                      }
-                    : a,
-                {}
-              )
-            )
-            .map(response => some(response))
+        )
       )
-    );
+    )
+  );
 
 /**
  * Converts a retrieved message to a message that can be shared via API
