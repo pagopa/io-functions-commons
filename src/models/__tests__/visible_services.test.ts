b77a4ccfa908d9e46d4d2a71802c6b22172a4029
Emanuele De Cupis <ema.decu@gmail.com>
2021-08-31 23:33:10 +0200

    add tests

---
+++ src/models/__tests__/visible_services.test.ts
@@ -0,0 +1,62 @@
+import { ServiceId } from "../../../generated/definitions/ServiceId";
+import { ServiceScopeEnum } from "../../../generated/definitions/ServiceScope";
+import { aVisibleService } from "../../../__mocks__/mocks";
+import {
+  toServicesPublic,
+  toServicesTuple,
+  VisibleService
+} from "../visible_service";
+
+const withScope = (scope: ServiceScopeEnum) => (
+  s: VisibleService
+): VisibleService => ({
+  ...s,
+  serviceMetadata: { ...s.serviceMetadata, scope }
+});
+
+const withId = (serviceId: ServiceId) => (
+  s: VisibleService
+): VisibleService => ({
+  ...s,
+  serviceId
+});
+
+const aVisibleServiceWithoutMetadata = aVisibleService;
+const aVisibleNationalService = withScope(ServiceScopeEnum.NATIONAL)(
+  aVisibleService
+);
+const aVisibleLocalService = withScope(ServiceScopeEnum.LOCAL)(aVisibleService);
+
+// this to check our assumptions on mock values
+expect(aVisibleServiceWithoutMetadata.serviceMetadata).not.toBeDefined();
+
+const anUnsortedMap = new Map()
+  .set("bbbb", withId("service1" as ServiceId)(aVisibleLocalService))
+  .set("cccc", withId("service2" as ServiceId)(aVisibleLocalService))
+  .set("aaaa", withId("service3" as ServiceId)(aVisibleLocalService));
+
+describe("toServicesTuple", () => {
+  it.each`
+    title                                                      | value                                                       | expected
+    ${"empty array on empty map"}                              | ${new Map()}                                                | ${[]}
+    ${"a national service from a service with no scope"}       | ${new Map().set("any-key", aVisibleServiceWithoutMetadata)} | ${[{ scope: ServiceScopeEnum.NATIONAL, service_id: aVisibleNationalService.serviceId, version: 1 }]}
+    ${"a national service from a service with national scope"} | ${new Map().set("any-key", aVisibleNationalService)}        | ${[{ scope: ServiceScopeEnum.NATIONAL, service_id: aVisibleNationalService.serviceId, version: 1 }]}
+    ${"a tuple sorted by key ascending"}                       | ${anUnsortedMap}                                            | ${[expect.objectContaining({ service_id: "service3" }), expect.objectContaining({ service_id: "service1" }), expect.objectContaining({ service_id: "service2" })]}
+  `("should return $title", ({ value, expected }) => {
+    const result = toServicesTuple(value);
+
+    expect(result).toEqual(expected);
+  });
+});
+
+describe("toServicesPublic", () => {
+  it.each`
+    title                                | value            | expected
+    ${"empty array on empty map"}        | ${new Map()}     | ${[]}
+    ${"a tuple sorted by key ascending"} | ${anUnsortedMap} | ${[expect.objectContaining({ service_id: "service3" }), expect.objectContaining({ service_id: "service1" }), expect.objectContaining({ service_id: "service2" })]}
+  `("should return $title", ({ value, expected }) => {
+    const result = toServicesTuple(value);
+
+    expect(result).toEqual(expected);
+  });
+});
