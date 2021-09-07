214722423d701e8a8a508cf635ce4ebf7d47fc53
Michael Disaro <15191694+michaeldisaro@users.noreply.github.com>
2021-09-07 11:16:24 +0200

    [#IP-362] Added middleware to map query params to optionals (#206)

---
+++ src/utils/middlewares/__tests__/optional_query_param.test.ts
@@ -0,0 +1,56 @@
+// eslint-disable @typescript-eslint/no-explicit-any
+
+import * as t from "io-ts";
+
+import { isLeft, isRight } from "fp-ts/lib/Either";
+import { isNone, isSome } from "fp-ts/lib/Option";
+
+import { OptionalQueryParamMiddleware } from "../optional_query_param";
+
+const middleware = OptionalQueryParamMiddleware("param", t.string);
+
+describe("OptionalQueryParamMiddleware", () => {
+  it("should respond with none if the query parameter is missing", async () => {
+    const result = await middleware({
+      query: {}
+    } as any);
+
+    expect(isRight(result)).toBeTruthy();
+    expect(isRight(result)).toBeTruthy();
+    if (isRight(result)) {
+      const maybeValue = result.right;
+      expect(isNone(maybeValue)).toBeTruthy();
+    }
+  });
+
+  it("should respond with a validation error if the query parameter is present but is NOT valid", async () => {
+    const result = await middleware({
+      query: {
+        param: 5
+      }
+    } as any);
+
+    expect(isLeft(result)).toBeTruthy();
+    if (isLeft(result)) {
+      expect(result.left.kind).toBe("IResponseErrorValidation");
+    }
+  });
+
+  it("should extract the query parameter if is present and valid", async () => {
+    const result = await middleware({
+      query: {
+        param: "hello"
+      }
+    } as any);
+
+    expect(isRight(result)).toBeTruthy();
+    if (isRight(result)) {
+      const maybeValue = result.right;
+      expect(isSome(maybeValue)).toBeTruthy();
+      if (isSome(maybeValue)) {
+        const value = maybeValue.value;
+        expect(value).toBe("hello");
+      }
+    }
+  });
+});
