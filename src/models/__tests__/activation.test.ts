cca22fa862a3beafddd123886241b6fb6b5e9c37
Daniele Manni <dan.manni@gmail.com>
2021-11-05 18:34:42 +0100

    Add new model Activation and some unit test

---
+++ src/models/__tests__/activation.test.ts
@@ -0,0 +1,224 @@
+import * as E from "fp-ts/Either";
+import * as O from "fp-ts/Option";
+
+import { Container } from "@azure/cosmos";
+import { NonNegativeInteger } from "@pagopa/ts-commons/lib/numbers";
+import { FiscalCode, NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { ActivationStatusEnum } from "../../../generated/definitions/ActivationStatus";
+import { generateComposedVersionedModelId } from "../../utils/cosmosdb_model_composed_versioned";
+import { Activation, ActivationModel, RetrievedActivation, ACTIVATION_REFERENCE_ID_FIELD, ACTIVATION_MODEL_PK_FIELD, NewActivation,  } from "../activation";
+const aFiscalCode = "FRLFRC74E04B157I" as FiscalCode;
+const aServiceId = "xyz" as NonEmptyString;
+const aRawActivation: Activation = {
+  status: ActivationStatusEnum.ACTIVE,
+  fiscalCode: aFiscalCode,
+  serviceId: aServiceId
+};
+const aFirstVersionId = generateComposedVersionedModelId<Activation, typeof ACTIVATION_REFERENCE_ID_FIELD, typeof ACTIVATION_MODEL_PK_FIELD>(aServiceId, aFiscalCode, 0 as NonNegativeInteger);
+const aRetrivedActivation: RetrievedActivation = {
+  _etag: "_etag",
+  _rid: "_rid",
+  _self: "_self",
+  _ts: 1,
+  id: aFirstVersionId,
+  kind: "IRetrievedActivation",
+  version: 0 as NonNegativeInteger,
+  ...aRawActivation
+};
+
+describe("findLastVersionByModelId", () => {
+  it("should resolve to an existing activation", async () => {
+    const containerMock = ({
+      items: {
+        create: jest.fn(),
+        query: jest.fn(() => ({
+          fetchAll: jest.fn(() =>
+            Promise.resolve({
+              resources: [aRetrivedActivation]
+            })
+          )
+        }))
+      }
+    } as unknown) as Container;
+
+    const model = new ActivationModel(containerMock);
+
+    const result = await model.findLastVersionByModelId([aServiceId, aFiscalCode])();
+
+    expect(E.isRight(result)).toBeTruthy();
+    if (E.isRight(result)) {
+      expect(O.isSome(result.right)).toBeTruthy();
+      expect(O.toUndefined(result.right)).toEqual({
+        ...aRetrivedActivation
+      });
+    }
+  });
+
+  it("should resolve to empty if activation was found", async () => {
+    const containerMock = ({
+      items: {
+        create: jest.fn(),
+        query: jest.fn(_ => ({
+          fetchAll: jest.fn(() =>
+            Promise.resolve({
+              resources: undefined
+            })
+          )
+        }))
+      }
+    } as unknown) as Container;
+
+    const model = new ActivationModel(containerMock);
+
+    const result = await model.findLastVersionByModelId([aServiceId, aFiscalCode])();
+
+    expect(E.isRight(result)).toBeTruthy();
+    if (E.isRight(result)) {
+      expect(O.isNone(result.right)).toBeTruthy();
+    }
+  });
+
+  it("should validate the retrieved object agains the model type", async () => {
+    const containerMock = ({
+      items: {
+        create: jest.fn(),
+        query: jest.fn(() => ({
+          fetchAll: jest.fn(() =>
+            Promise.resolve({
+              resources: [{}]
+            })
+          )
+        }))
+      }
+    } as unknown) as Container;
+
+    const model = new ActivationModel(containerMock);
+
+    const result = await model.findLastVersionByModelId([aServiceId, aFiscalCode])();
+
+    expect(E.isLeft(result)).toBeTruthy();
+    if (E.isLeft(result)) {
+      expect(result.left.kind).toBe("COSMOS_DECODING_ERROR");
+    }
+  });
+});
+
+describe("create", () => {
+  it("should create a new activation", async () => {
+    const mockCreate = jest.fn((_, __) => Promise.resolve({
+      resource: aRetrivedActivation
+    }));
+    const containerMock = ({
+      items: {
+        create: mockCreate,
+        query: jest.fn(() => ({
+          fetchAll: jest.fn()
+        }))
+      }
+    } as unknown) as Container;
+
+    const model = new ActivationModel(containerMock);
+
+    const newActivation: NewActivation = {
+      fiscalCode: aFiscalCode,
+      serviceId: aServiceId,
+      status: ActivationStatusEnum.ACTIVE,
+      kind: "INewActivation"
+    };
+
+    const result = await model.create(newActivation)();
+
+    expect(mockCreate).toHaveBeenCalledTimes(1);
+    expect(mockCreate.mock.calls[0][0]).toEqual({...newActivation, id: aFirstVersionId, version: 0});
+    expect(E.isRight(result)).toBeTruthy();
+    if (E.isRight(result)) {
+      expect(result.right.fiscalCode).toEqual(newActivation.fiscalCode);
+      expect(result.right.serviceId).toEqual(newActivation.serviceId);
+      expect(result.right.id).toEqual(aFirstVersionId);
+      expect(result.right.version).toEqual(0);
+    }
+  });
+});
+
+describe("upsert", () => {
+  it("should create a new activation with version 0 if don't exists previous documents", async () => {
+    const mockCreate = jest.fn((_, __) => Promise.resolve({
+      resource: aRetrivedActivation
+    }));
+    const containerMock = ({
+      items: {
+        create: mockCreate,
+        query: jest.fn(() => ({
+          fetchAll: jest.fn(() => Promise.resolve({
+            resources: undefined
+          }))
+        }))
+      }
+    } as unknown) as Container;
+
+    const model = new ActivationModel(containerMock);
+
+    const newActivation: NewActivation = {
+      fiscalCode: aFiscalCode,
+      serviceId: aServiceId,
+      status: ActivationStatusEnum.ACTIVE,
+      kind: "INewActivation"
+    };
+
+    const result = await model.upsert(newActivation)();
+
+    expect(mockCreate).toHaveBeenCalledTimes(1);
+    expect(mockCreate.mock.calls[0][0]).toEqual({...newActivation, id: aFirstVersionId, version: 0});
+    expect(E.isRight(result)).toBeTruthy();
+    if (E.isRight(result)) {
+      expect(result.right.fiscalCode).toEqual(newActivation.fiscalCode);
+      expect(result.right.serviceId).toEqual(newActivation.serviceId);
+      expect(result.right.id).toEqual(aFirstVersionId);
+      expect(result.right.version).toEqual(0);
+    }
+  });
+
+  it("should create a new activation with version 1 if exists a previous document", async () => {
+    const expectedDocumentId = generateComposedVersionedModelId<Activation, typeof ACTIVATION_REFERENCE_ID_FIELD, typeof ACTIVATION_MODEL_PK_FIELD>(aServiceId, aFiscalCode, 1 as NonNegativeInteger);
+    const mockCreate = jest.fn((_, __) => Promise.resolve({
+      resource: {
+        ...aRetrivedActivation,
+        version: 1,
+        id: expectedDocumentId
+      }
+    }));
+    const containerMock = ({
+      items: {
+        create: mockCreate,
+        query: jest.fn(() => ({
+          fetchAll: jest.fn(() => Promise.resolve({
+            resources: [
+              aRetrivedActivation
+            ]
+          }))
+        }))
+      }
+    } as unknown) as Container;
+
+    const model = new ActivationModel(containerMock);
+
+    const newActivation: NewActivation = {
+      fiscalCode: aFiscalCode,
+      serviceId: aServiceId,
+      status: ActivationStatusEnum.ACTIVE,
+      kind: "INewActivation"
+    };
+
+    const result = await model.upsert(newActivation)();
+
+    expect(mockCreate).toHaveBeenCalledTimes(1);
+    expect(mockCreate.mock.calls[0][0]).toEqual({...newActivation, id: expectedDocumentId, version: 1});
+    expect(E.isRight(result)).toBeTruthy();
+    if (E.isRight(result)) {
+      expect(result.right.fiscalCode).toEqual(newActivation.fiscalCode);
+      expect(result.right.serviceId).toEqual(newActivation.serviceId);
+      expect(result.right.version).toEqual(1);
+      expect(result.right.id).toEqual(expectedDocumentId);
+    }
+  });
+});
---
+++ src/models/activation.ts
@@ -0,0 +1,63 @@
+import { Container } from "@azure/cosmos";
+import { FiscalCode, NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import * as t from "io-ts";
+import { ActivationStatus } from "../../generated/definitions/ActivationStatus";
+import { CosmosdbModelComposedVersioned } from "../utils/cosmosdb_model_composed_versioned";
+import { RetrievedVersionedModel } from "../utils/cosmosdb_model_versioned";
+import { wrapWithKind } from "../utils/types";
+import { PROFILE_MODEL_PK_FIELD } from "./profile";
+import { SERVICE_MODEL_ID_FIELD } from "./service";
+
+export const ACTIVATION_COLLECTION_NAME = "activations";
+export const ACTIVATION_REFERENCE_ID_FIELD = SERVICE_MODEL_ID_FIELD;
+export const ACTIVATION_MODEL_PK_FIELD = PROFILE_MODEL_PK_FIELD;
+
+const ActivationR = t.interface({
+  fiscalCode: FiscalCode,
+  serviceId: NonEmptyString,
+  status: ActivationStatus
+});
+
+const ActivationO = t.partial({});
+
+export type Activation = t.TypeOf<typeof Activation>;
+export const Activation = t.intersection(
+  [ActivationR, ActivationO],
+  "Activation"
+);
+
+export type NewActivation = t.TypeOf<typeof NewActivation>;
+export const NewActivation = wrapWithKind(
+  Activation,
+  "INewActivation" as const
+);
+
+export const RetrievedActivation = wrapWithKind(
+  t.intersection([Activation, RetrievedVersionedModel]),
+  "IRetrievedActivation" as const
+);
+
+export type RetrievedActivation = t.TypeOf<typeof RetrievedActivation>;
+
+export class ActivationModel extends CosmosdbModelComposedVersioned<
+  Activation,
+  NewActivation,
+  RetrievedActivation,
+  typeof ACTIVATION_REFERENCE_ID_FIELD,
+  typeof ACTIVATION_MODEL_PK_FIELD
+> {
+  /**
+   * Creates a new Activation model
+   *
+   * @param container the Cosmos container client
+   */
+  constructor(container: Container) {
+    super(
+      container,
+      NewActivation,
+      RetrievedActivation,
+      ACTIVATION_REFERENCE_ID_FIELD,
+      ACTIVATION_MODEL_PK_FIELD
+    );
+  }
+}
