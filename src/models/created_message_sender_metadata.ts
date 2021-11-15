9b2c7d7ceace22d4c51e56d31e6e4346169a644d
Daniele Manni <dan.manni@gmail.com>
2021-11-15 17:27:55 +0100

    [#IC-89] serviceCategory in CreatedMessageEventSenderMetadata

--- src/models/created_message_sender_metadata.ts
+++ src/models/created_message_sender_metadata.ts
@@ -2,11 +2,17 @@ import * as t from "io-ts";
 
 import { EmailString, NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { OrganizationFiscalCode } from "../../generated/definitions/OrganizationFiscalCode";
+import { ServiceCategory } from "../../generated/definitions/ServiceCategory";
+import { withDefault } from "@pagopa/ts-commons/lib/types";
+import { StandardServiceCategoryEnum } from "../../generated/definitions/StandardServiceCategory";
 
 /**
  * Sender metadata associated to a message
  */
 export const CreatedMessageEventSenderMetadata = t.interface({
+  // withDefault is required for old messages already sent with missing serviceCategory
+  // to prevent decode errors on running orchestrators. The default value is STANDARD.
+  serviceCategory: withDefault(ServiceCategory, StandardServiceCategoryEnum.STANDARD),
   departmentName: NonEmptyString,
   organizationFiscalCode: OrganizationFiscalCode,
   organizationName: NonEmptyString,
