1595c43488bf704e10fb7820b48c7aff6ec46221
gquadrati <greta.quadrati@pagopa.it>
2022-03-07 11:16:46 +0100

    [#IC-313] add other integration tests

--- __integrations__/models/message-status.test.ts
+++ __integrations__/models/message-status.test.ts
@@ -1,11 +1,13 @@
 /* eslint-disable no-console */
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 
+import * as E from "fp-ts/Either";
 import * as TE from "fp-ts/TaskEither";
 import * as O from "fp-ts/Option";
 import * as RA from "fp-ts/ReadonlyArray";
 
 import {
+  getMessageStatusUpdater,
   MessageStatusModel,
   MESSAGE_STATUS_MODEL_PK_FIELD
 } from "../../src/models/message_status";
@@ -36,7 +38,7 @@ export const oldMessageStatusList = Array.from(
 );
 
 describe("Models |> Message-Status", () => {
-  it("should retrieve message-status with isRead and isArchived", async () => {
+  it("should retrieve message-status with default isRead and isArchived when not present", async () => {
     const context = await createContext(MESSAGE_STATUS_MODEL_PK_FIELD);
     await context.init();
     const model = new MessageStatusModel(context.container);
@@ -67,6 +69,135 @@ describe("Models |> Message-Status", () => {
       isRead: false,
       isArchived: false
     });
+
+    context.dispose();
+  });
+
+  it("should read a message-status with isRead and isArchived, when present", async () => {
+    const context = await createContext(MESSAGE_STATUS_MODEL_PK_FIELD);
+    await context.init();
+    const model = new MessageStatusModel(context.container);
+
+    await pipe(
+      oldMessageStatusList,
+      RA.map(ms =>
+        TE.tryCatch<CosmosErrors, any>(
+          () =>
+            context.container.items.create(
+              { ...ms, isRead: true, isArchived: true },
+              {
+                disableAutomaticIdGeneration: true
+              }
+            ),
+          toCosmosErrorResponse
+        )
+      ),
+      RA.sequence(TE.ApplicativePar),
+      TE.mapLeft(err => fail(`Cannot insert items ${err}`))
+    )();
+
+    const retrievedValue = await pipe(
+      model.findLastVersionByModelId([oldMessageStatusList[0].messageId]),
+      TE.map(O.getOrElseW(() => fail("MessageStatus not found"))),
+      TE.getOrElse(() => fail("Cosmos error"))
+    )();
+
+    expect(retrievedValue).toMatchObject({
+      ...oldMessageStatusList[0],
+      isRead: true,
+      isArchived: true
+    });
+
+    context.dispose();
+  });
+  it("should create a message-status using getMessageStatusUpdater", async () => {
+    const context = await createContext(MESSAGE_STATUS_MODEL_PK_FIELD);
+    await context.init();
+    const model = new MessageStatusModel(context.container);
+
+    const anotherMessageId = "ANOTHER_MESSAGE_ID" as NonEmptyString;
+
+    const updater = getMessageStatusUpdater(model, anotherMessageId);
+    const result = await updater(MessageStatusValueEnum.ACCEPTED)();
+
+    expect(E.isRight(result)).toBe(true);
+
+    if (E.isRight(result)) {
+      expect(result.right).toMatchObject({
+        messageId: anotherMessageId,
+        id: `${anotherMessageId}-${"0".repeat(16)}` as NonEmptyString,
+        version: 0,
+        isRead: false,
+        isArchived: false
+      });
+    }
+
+    context.dispose();
+  });
+  it("should upsert a message-status using getMessageStatusUpdater", async () => {
+    const context = await createContext(MESSAGE_STATUS_MODEL_PK_FIELD);
+    await context.init();
+    const model = new MessageStatusModel(context.container);
+
+    const anotherMessageId = "ANOTHER_MESSAGE_ID" as NonEmptyString;
+
+    const updater = getMessageStatusUpdater(model, anotherMessageId);
+
+    //First, create the "Accepted" status
+    await updater(MessageStatusValueEnum.ACCEPTED)();
+    // Then upsert with "Processed" status
+    const result = await updater(MessageStatusValueEnum.PROCESSED)();
+
+    expect(E.isRight(result)).toBe(true);
+
+    if (E.isRight(result)) {
+      expect(result.right).toMatchObject({
+        messageId: anotherMessageId,
+        id: `${anotherMessageId}-${"0".repeat(15)}1` as NonEmptyString,
+        version: 1,
+        isRead: false,
+        isArchived: false
+      });
+    }
+
+    context.dispose();
+  });
+
+  it("should upsert a message-status with isRead and isArchived", async () => {
+    const context = await createContext(MESSAGE_STATUS_MODEL_PK_FIELD);
+    await context.init();
+    const model = new MessageStatusModel(context.container);
+
+    const anotherMessageId = "ANOTHER_MESSAGE_ID" as NonEmptyString;
+
+    const updater = getMessageStatusUpdater(model, anotherMessageId);
+
+    //First, create the "Accepted" status
+    const accepted = await updater(MessageStatusValueEnum.ACCEPTED)();
+
+    expect(E.isRight(accepted)).toBe(true);
+    if (E.isRight(accepted)) {
+      await model.upsert({
+        ...accepted.right,
+        isRead: true,
+        isArchived: true,
+        kind: "INewMessageStatus"
+      })();
+
+      const retrievedValue = await pipe(
+        model.findLastVersionByModelId([anotherMessageId]),
+        TE.map(O.getOrElseW(() => fail("MessageStatus not found"))),
+        TE.getOrElse(() => fail("Cosmos error"))
+      )();
+
+      expect(retrievedValue).toMatchObject({
+        messageId: anotherMessageId,
+        id: `${anotherMessageId}-${"0".repeat(15)}1`,
+        version: 1,
+        isRead: true,
+        isArchived: true
+      });
+    }
 
     context.dispose();
   });
