46816300f13e9553a8350aff004adabe27d753d0
fabriziopapi <80677434+fabriziopapi@users.noreply.github.com>
2022-03-15 12:00:15 +0100

    [#IC-304] Add message_view model (#242)
    * [#IC-304] (+) add message_view model
    
    * [#IC-304] (+) fixed typo and removed serviceName from message view
    
    * [#IC-304] (+) add message view model test

---
+++ src/models/__tests__/message_view.test.ts
@@ -0,0 +1,86 @@
+import * as E from "fp-ts/lib/Either";
+
+import { Container } from "@azure/cosmos";
+import {
+  FiscalCode,
+  NonEmptyString,
+  OrganizationFiscalCode
+} from "@pagopa/ts-commons/lib/strings";
+import {
+  MessageView,
+  RetrievedMessageView,
+  Components,
+  Status
+} from "../message_view";
+import { ServiceId } from "../../../generated/definitions/ServiceId";
+import { MessageStatusValueEnum } from "../../../generated/definitions/MessageStatusValue";
+import { NonNegativeInteger } from "@pagopa/ts-commons/lib/numbers";
+import { MessageViewModel } from "../message_view";
+
+const aComponents: Components = {
+  attachments: { has: false },
+  euCovidCert: { has: false },
+  legalData: { has: false },
+  payment: { has: false, rptId: "" }
+};
+
+const aStatus: Status = {
+  archived: false,
+  processing: MessageStatusValueEnum.PROCESSED,
+  read: false
+};
+
+const aMessageView: MessageView = {
+  components: aComponents,
+  createdAt: new Date(),
+  fiscalCode: "AAAAAA00A00A000A" as FiscalCode,
+  id: "a-unique-msg-id" as NonEmptyString,
+  messageTitle: "a-msg-title" as NonEmptyString,
+  organizationName: "an-organization-name" as NonEmptyString,
+  senderServiceId: "a-service-id" as ServiceId,
+  status: aStatus,
+  version: 0 as NonNegativeInteger
+};
+
+const aRetrievedMessageView: RetrievedMessageView = {
+  ...aMessageView,
+  _etag: "_etag",
+  _rid: "_rid",
+  _self: "_self",
+  _ts: 1
+};
+
+const mockFetchAll = jest.fn();
+const mockGetAsyncIterator = jest.fn();
+const mockCreate = jest.fn();
+
+const containerMock = ({
+  items: {
+    readAll: jest.fn(() => ({
+      fetchAll: mockFetchAll,
+      getAsyncIterator: mockGetAsyncIterator
+    })),
+    create: mockCreate,
+    query: jest.fn(() => ({
+      fetchAll: mockFetchAll
+    }))
+  }
+} as unknown) as Container;
+
+describe("create", () => {
+  it("GIVEN a valid message_view WHEN the client create is called THEN the create return a Right", async () => {
+    mockCreate.mockImplementationOnce((_, __) =>
+      Promise.resolve({
+        resource: { ...aRetrievedMessageView }
+      })
+    );
+    const model = new MessageViewModel(containerMock);
+    const result = await model.create(aMessageView)();
+    expect(mockCreate).toBeCalled();
+    expect(mockCreate).toBeCalledWith(
+      JSON.parse(JSON.stringify(aMessageView)),
+      expect.objectContaining({})
+    );
+    expect(E.isRight(result)).toBeTruthy();
+  });
+});
---
+++ src/models/message_view.ts
@@ -0,0 +1,80 @@
+import * as t from "io-ts";
+import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { Container } from "@azure/cosmos";
+import { NonNegativeInteger } from "@pagopa/ts-commons/lib/numbers";
+import { FiscalCode } from "../../generated/definitions/FiscalCode";
+import { MessageStatusValue } from "../../generated/definitions/MessageStatusValue";
+import { ServiceId } from "../../generated/definitions/ServiceId";
+import { TimeToLiveSeconds } from "../../generated/definitions/TimeToLiveSeconds";
+import { Timestamp } from "../../generated/definitions/Timestamp";
+import { CosmosResource, CosmosdbModel } from "../utils/cosmosdb_model";
+
+export const MESSAGE_VIEW_COLLECTION_NAME = "message-view";
+const MESSAGE_VIEW_MODEL_PK_FIELD = "fiscalCode";
+
+export const Component = t.interface({
+  has: t.boolean
+});
+export type Component = t.TypeOf<typeof Component>;
+
+export const PaymentComponent = t.intersection([
+  Component,
+  t.interface({ rptId: t.string })
+]);
+export type PaymentComponent = t.TypeOf<typeof PaymentComponent>;
+
+export const Components = t.interface({
+  attachments: Component,
+  euCovidCert: Component,
+  legalData: Component,
+  payment: PaymentComponent
+});
+export type Components = t.TypeOf<typeof Components>;
+
+export const Status = t.interface({
+  archived: t.boolean,
+  processing: MessageStatusValue,
+  read: t.boolean
+});
+export type Status = t.TypeOf<typeof Status>;
+
+const MessageViewR = t.interface({
+  components: Components,
+  createdAt: Timestamp,
+  fiscalCode: FiscalCode,
+  id: NonEmptyString,
+  messageTitle: NonEmptyString,
+  organizationName: NonEmptyString,
+  senderServiceId: ServiceId,
+  status: Status,
+  version: NonNegativeInteger
+});
+
+const MessageViewO = t.partial({
+  timeToLive: TimeToLiveSeconds
+});
+
+export const MessageView = t.intersection([MessageViewR, MessageViewO]);
+export type MessageView = t.TypeOf<typeof MessageView>;
+
+export const RetrievedMessageView = t.intersection([
+  MessageView,
+  CosmosResource
+]);
+export type RetrievedMessageView = t.TypeOf<typeof RetrievedMessageView>;
+
+export class MessageViewModel extends CosmosdbModel<
+  MessageView,
+  MessageView,
+  RetrievedMessageView,
+  typeof MESSAGE_VIEW_MODEL_PK_FIELD
+> {
+  /**
+   * Creates a new MessageView model
+   *
+   * @param container the Cosmos container client
+   */
+  constructor(container: Container) {
+    super(container, MessageView, RetrievedMessageView);
+  }
+}
