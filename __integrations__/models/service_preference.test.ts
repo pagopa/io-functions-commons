922547b72f6dbe1f6b5e5486f2af16fbf7c9d7e5
Michael Disaro <michael.disaro@gmail.com>
2022-05-18 14:10:50 +0200

    added integration tests

---
+++ __integrations__/models/service_preference.test.ts
@@ -0,0 +1,176 @@
+/* eslint-disable no-console */
+import { FiscalCode, NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+
+import * as TE from "fp-ts/lib/TaskEither";
+import * as E from "fp-ts/lib/Either";
+import {
+  ServicePreference,
+  SERVICE_PREFERENCES_MODEL_PK_FIELD,
+  SERVICE_PREFERENCES_COLLECTION_NAME,
+  ServicesPreferencesModel,
+  NewServicePreference
+} from "../../src/models/service_preference";
+import { createContext } from "./cosmos_utils";
+import { pipe } from "fp-ts/lib/function";
+import { ServiceScopeEnum } from "../../generated/definitions/ServiceScope";
+import {
+  CosmosErrors,
+  DocumentSearchKey,
+  toCosmosErrorResponse
+} from "../../src/utils/cosmosdb_model";
+import { generateVersionedModelId } from "../../src/utils/cosmosdb_model_versioned";
+import { NonNegativeInteger } from "@pagopa/ts-commons/lib/numbers";
+import { StandardServiceCategoryEnum } from "../../generated/definitions/StandardServiceCategory";
+import { withoutUndefinedValues } from "@pagopa/ts-commons/lib/types";
+
+const aFiscalCode = "FRLFRC74E04B157I" as FiscalCode;
+const aServiceId = "aServiceId" as NonEmptyString;
+
+const aServicePreference: ServicePreference = pipe(
+  ServicePreference.decode({
+    fiscalCode: aFiscalCode,
+    serviceId: aServiceId,
+    isAllowSendReadMessageStatus: true,
+    isEmailEnabled: true,
+    isInboxEnabled: true,
+    settingsVersion: 0 as NonNegativeInteger,
+    isWebhookEnabled: true
+  }),
+  E.getOrElseW(() => {
+    throw new Error("Cannot decode service payload.");
+  })
+);
+
+describe("Models |> ServicePreference", () => {
+  it("should save and retrieve documents", async () => {
+    const context = createContext(SERVICE_PREFERENCES_MODEL_PK_FIELD);
+    await context.init();
+    const model = new ServicesPreferencesModel(
+      context.container,
+      SERVICE_PREFERENCES_COLLECTION_NAME
+    );
+
+    const newDoc = {
+      kind: "INewServicePreference" as const,
+      ...aServicePreference
+    } as NewServicePreference;
+
+    // create a new document
+    const created = await pipe(
+      model.create(newDoc),
+      TE.bimap(
+        _ => fail(`Failed to create doc, error: ${JSON.stringify(_)}`),
+        result => {
+          expect(result).toEqual(
+            expect.objectContaining({
+              ...aServicePreference
+            })
+          );
+          return result;
+        }
+      ),
+      TE.toUnion
+    )();
+
+    // upsert document changing isAllowSendReadMessageStatus
+    await pipe(
+      model.upsert({
+        ...created,
+        kind: "INewServicePreference",
+        isAllowSendReadMessageStatus: false
+      }),
+      TE.bimap(
+        _ => fail(`Failed to update doc, error: ${JSON.stringify(_)}`),
+        result => {
+          expect(result).toEqual(
+            expect.objectContaining({
+              ...aServicePreference,
+              isAllowSendReadMessageStatus: false
+            })
+          );
+        }
+      )
+    )();
+
+    // get document by id
+    const documentSearchKey = [
+      created.id,
+      created[SERVICE_PREFERENCES_MODEL_PK_FIELD]
+    ] as DocumentSearchKey<ServicePreference, "id", "fiscalCode">;
+    await pipe(
+      model.find(documentSearchKey),
+      TE.chainW(maybeServicePreference =>
+        TE.fromOption(() => "It's none")(maybeServicePreference)
+      ),
+      TE.bimap(
+        _ => fail(`Failed to read doc, error: ${JSON.stringify(_)}`),
+        result => {
+          expect(result).toEqual(
+            expect.objectContaining({
+              ...aServicePreference,
+              isAllowSendReadMessageStatus: false
+            })
+          );
+        }
+      )
+    )();
+
+    context.dispose();
+  });
+
+  it("should read documents with default isAllowSendReadMessageStatus", async () => {
+    const context = createContext(SERVICE_PREFERENCES_MODEL_PK_FIELD);
+    await context.init();
+    const model = new ServicesPreferencesModel(
+      context.container,
+      SERVICE_PREFERENCES_COLLECTION_NAME
+    );
+
+    const {
+      isAllowSendReadMessageStatus,
+      ...aServicePreferenceWithoutIsAllowSendReadMessageStatus
+    } = aServicePreference;
+
+    const partialDocumentToSave = {
+      ...aServicePreferenceWithoutIsAllowSendReadMessageStatus,
+      id: "aServicePreferenceDocumentId"
+    };
+
+    // create a new document
+    const createTestServicePreferenceDoc = await pipe(
+      TE.tryCatch(
+        () =>
+          context.container.items.create(
+            withoutUndefinedValues(partialDocumentToSave),
+            { disableAutomaticIdGeneration: true }
+          ),
+        E.toError
+      )
+    )();
+
+    // get document by id
+    const documentSearchKey = [
+      partialDocumentToSave.id,
+      partialDocumentToSave[SERVICE_PREFERENCES_MODEL_PK_FIELD]
+    ] as DocumentSearchKey<ServicePreference, "id", "fiscalCode">;
+    await pipe(
+      model.find(documentSearchKey),
+      TE.chainW(maybeServicePreference =>
+        TE.fromOption(() => "It's none")(maybeServicePreference)
+      ),
+      TE.bimap(
+        _ => fail(`Failed to read doc, error: ${JSON.stringify(_)}`),
+        result => {
+          expect(result).toEqual(
+            expect.objectContaining({
+              ...aServicePreference,
+              isAllowSendReadMessageStatus: true
+            })
+          );
+        }
+      )
+    )();
+
+    context.dispose();
+  });
+});
