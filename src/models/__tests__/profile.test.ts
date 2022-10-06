01ca17c56b8311ab4d46ca4fd0b40cbf5d8f99bd
arcogabbo <arcogabbo@gmail.com>
2022-10-06 11:14:10 +0200

    [#IOCIT-117] ported old commented tests and added new ones

--- src/models/__tests__/profile.test.ts
+++ src/models/__tests__/profile.test.ts
@@ -1,12 +1,14 @@
 import * as E from "fp-ts/lib/Either";
 import * as O from "fp-ts/lib/Option";
+import { mocked } from "ts-jest/utils";
 
 import { NonNegativeInteger } from "@pagopa/ts-commons/lib/numbers";
 import { readableReport } from "@pagopa/ts-commons/lib/reporters";
-import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { EmailString, NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { FiscalCode } from "../../../generated/definitions/FiscalCode";
 
 import {
+  NewProfile,
   Profile,
   ProfileModel,
   PROFILE_SERVICE_PREFERENCES_SETTINGS_LEGACY_VERSION,
@@ -39,7 +41,7 @@ const aRetrievedProfile: RetrievedProfile = {
   _rid: "_rid",
   _self: "_self",
   _ts: 1,
-  id: (aFiscalCode as unknown) as NonEmptyString,
+  id: (`${aFiscalCode}-${"0".repeat(16)}` as unknown) as NonEmptyString,
   kind: "IRetrievedProfile",
   version: 0 as NonNegativeInteger,
   ...aStoredProfile
@@ -184,147 +186,153 @@ describe("Profile codec", () => {
   });
 });
 
-/*
 describe("createProfile", () => {
+  const newProfile: NewProfile = {
+    kind: "INewProfile",
+    ...aRawProfile,
+    servicePreferencesSettings: {
+      mode: ServicesPreferencesModeEnum.LEGACY,
+      version: -1
+    }
+  };
+
   it("should create a new profile", async () => {
-    const clientMock: any = {
-      createDocument: jest.fn((_, newDocument, __, cb) => {
-        cb(undefined, {
-          ...newDocument,
-          _self: "self",
-          _ts: 123
-        });
-      })
-    };
+    const containerMock = ({
+      items: {
+        create: jest.fn().mockReturnValue(
+          Promise.resolve({
+            resource: aRetrievedProfile
+          })
+        )
+      }
+    } as unknown) as Container;
 
-    const model = new ProfileModel(clientMock, profilesCollectionUrl);
+    const model = new ProfileModel(containerMock);
 
-    const newProfile: Profile = {
-      fiscalCode: aFiscalCode
-    };
+    const result = await model.create(newProfile)();
 
-    const result = await model.create(newProfile, newProfile.fiscalCode);
-
-    expect(clientMock.createDocument).toHaveBeenCalledTimes(1);
-    expect(clientMock.createDocument.mock.calls[0][1].kind).toBeUndefined();
-    expect(clientMock.createDocument.mock.calls[0][2]).toHaveProperty(
-      "partitionKey",
-      aFiscalCode
+    expect(containerMock.items.create).toHaveBeenCalledTimes(1);
+    expect(mocked(containerMock.items.create).mock.calls[0][0].kind).toEqual(
+      "INewProfile"
     );
-    expect(isRight(result)).toBeTruthy();
-    if (isRight(result)) {
-      expect(result.value.fiscalCode).toEqual(newProfile.fiscalCode);
-      expect(result.value.id).toEqual(`${aFiscalCode}-${"0".repeat(16)}`);
-      expect(result.value.version).toEqual(0);
-      expect(result.value.isTestProfile).toEqual(false);
+    expect(mocked(containerMock.items.create).mock.calls[0][0]).toHaveProperty(
+      "fiscalCode",
+      newProfile.fiscalCode
+    );
+    expect(E.isRight(result)).toBeTruthy();
+    if (E.isRight(result)) {
+      expect(result.right.fiscalCode).toEqual(newProfile.fiscalCode);
+      expect(result.right.id).toEqual(`${aFiscalCode}-${"0".repeat(16)}`);
+      expect(result.right.version).toEqual(0);
+      expect(result.right.isTestProfile).toEqual(false);
     }
   });
 
   it("should reject the promise in case of error", async () => {
-    const clientMock: any = {
-      createDocument: jest.fn((_, __, ___, cb) => {
-        cb("error");
-      })
-    };
+    const containerMock = ({
+      items: {
+        create: jest.fn().mockReturnValue(Promise.reject())
+      }
+    } as unknown) as Container;
 
-    const model = new ProfileModel(clientMock, profilesCollectionUrl);
+    const model = new ProfileModel(containerMock);
 
-    const newProfile: Profile = {
-      fiscalCode: aFiscalCode
-    };
+    const result = await model.create(newProfile)();
 
-    const result = await model.create(newProfile, newProfile.fiscalCode);
+    expect(containerMock.items.create).toHaveBeenCalledTimes(1);
 
-    expect(clientMock.createDocument).toHaveBeenCalledTimes(1);
-
-    expect(isLeft(result)).toBeTruthy();
-    if (isLeft(result)) {
-      expect(result.value).toEqual("error");
+    expect(E.isLeft(result)).toBeTruthy();
+    if (E.isLeft(result)) {
+      expect(result.left.kind).toEqual("COSMOS_ERROR_RESPONSE");
     }
   });
 });
 
-describe("update", () => {
+describe("updateProfile", () => {
   it("should update an existing profile", async () => {
-    const clientMock: any = {
-      createDocument: jest.fn((_, newDocument, __, cb) => {
-        cb(undefined, {
-          ...newDocument,
-          _self: "self",
-          _ts: 123
-        });
-      }),
-      readDocument: jest.fn((_, __, cb) => cb(undefined, aRetrievedProfile))
+    const containerMock = ({
+      items: {
+        create: jest.fn().mockReturnValue(
+          Promise.resolve({
+            resource: {
+              ...aRetrievedProfile,
+              id: `${aFiscalCode}-${"0".repeat(15)}1`,
+              version: 1,
+              email: "new@example.com"
+            }
+          })
+        )
+      }
+    } as unknown) as Container;
+
+    const model = new ProfileModel(containerMock);
+
+    const aProfileWithDifferentEmail = {
+      ...aRetrievedProfile,
+      email: "new@example.com" as EmailString
     };
 
-    const model = new ProfileModel(clientMock, profilesCollectionUrl);
+    const result = await model.update(aProfileWithDifferentEmail)();
 
-    const result = await model.update(
-      aRetrievedProfile.fiscalCode,
-      aRetrievedProfile.fiscalCode,
-      p => {
-        return {
-          ...p,
-          email: "new@example.com" as EmailString
-        };
-      }
+    expect(containerMock.items.create).toHaveBeenCalledTimes(1);
+    expect(mocked(containerMock.items.create).mock.calls[0][0].kind).toEqual(
+      "INewProfile"
     );
-
-    expect(clientMock.createDocument).toHaveBeenCalledTimes(1);
-    expect(clientMock.createDocument.mock.calls[0][1].kind).toBeUndefined();
-    expect(clientMock.createDocument.mock.calls[0][2]).toHaveProperty(
-      "partitionKey",
+    expect(mocked(containerMock.items.create).mock.calls[0][0]).toHaveProperty(
+      "fiscalCode",
       aFiscalCode
     );
-    expect(isRight(result)).toBeTruthy();
-    if (isRight(result)) {
-      expect(result.value.isSome()).toBeTruthy();
-      if (isSome(result.value)) {
-        const updatedProfile = result.value.value;
-        expect(updatedProfile.fiscalCode).toEqual(aRetrievedProfile.fiscalCode);
-        expect(updatedProfile.id).toEqual(`${aFiscalCode}-${"0".repeat(15)}1`);
-        expect(updatedProfile.version).toEqual(1);
-        expect(updatedProfile.email).toEqual("new@example.com");
+    expect(mocked(containerMock.items.create).mock.calls[0][0]).toHaveProperty(
+      "email",
+      aProfileWithDifferentEmail.email
+    );
+    expect(E.isRight(result)).toBeTruthy();
+    if (E.isRight(result)) {
+      const updatedProfile = result.right;
+      expect(updatedProfile.fiscalCode).toEqual(aRetrievedProfile.fiscalCode);
+      expect(updatedProfile.id).toEqual(`${aFiscalCode}-${"0".repeat(15)}1`);
+      expect(updatedProfile.version).toEqual(1);
+      expect(updatedProfile.email).toEqual("new@example.com");
+    }
+  });
+
+  it("should get COSMOS_ERROR_RESPONSE in case of create error", async () => {
+    const containerMock = ({
+      items: {
+        create: jest.fn().mockReturnValue(Promise.reject())
       }
+    } as unknown) as Container;
+
+    const model = new ProfileModel(containerMock);
+
+    const result = await model.update(aRetrievedProfile)();
+
+    expect(containerMock.items.create).toHaveBeenCalledTimes(1);
+
+    expect(E.isLeft(result)).toBeTruthy();
+    if (E.isLeft(result)) {
+      expect(result.left.kind).toEqual("COSMOS_ERROR_RESPONSE");
     }
   });
 
-  it("should reject the promise in case of error (read)", async () => {
-    const clientMock: any = {
-      createDocument: jest.fn(),
-      readDocument: jest.fn((_, __, cb) => cb("error"))
-    };
+  it("should get COSMOS_EMPTY_RESPONSE in case of empty resource", async () => {
+    const containerMock = ({
+      items: {
+        create: jest
+          .fn()
+          .mockReturnValue(Promise.resolve({ resource: undefined }))
+      }
+    } as unknown) as Container;
 
-    const model = new ProfileModel(clientMock, profilesCollectionUrl);
+    const model = new ProfileModel(containerMock);
 
-    const result = await model.update(aFiscalCode, aFiscalCode, o => o);
+    const result = await model.update(aRetrievedProfile)();
 
-    expect(clientMock.readDocument).toHaveBeenCalledTimes(1);
-    expect(clientMock.createDocument).not.toHaveBeenCalled();
+    expect(containerMock.items.create).toHaveBeenCalledTimes(1);
 
-    expect(isLeft(result)).toBeTruthy();
-    if (isLeft(result)) {
-      expect(result.value).toEqual("error");
-    }
-  });
-
-  it("should reject the promise in case of error (create)", async () => {
-    const clientMock: any = {
-      createDocument: jest.fn((_, __, ___, cb) => cb("error")),
-      readDocument: jest.fn((_, __, cb) => cb(undefined, aRetrievedProfile))
-    };
-
-    const model = new ProfileModel(clientMock, profilesCollectionUrl);
-
-    const result = await model.update(aFiscalCode, aFiscalCode, o => o);
-
-    expect(clientMock.readDocument).toHaveBeenCalledTimes(1);
-    expect(clientMock.createDocument).toHaveBeenCalledTimes(1);
-
-    expect(isLeft(result)).toBeTruthy();
-    if (isLeft(result)) {
-      expect(result.value).toEqual("error");
+    expect(E.isLeft(result)).toBeTruthy();
+    if (E.isLeft(result)) {
+      expect(result.left.kind).toEqual("COSMOS_EMPTY_RESPONSE");
     }
   });
 });
-*/
