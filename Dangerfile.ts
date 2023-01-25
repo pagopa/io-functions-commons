6d49fc73b8e13d7ccbb336c7163d652649a63432
Michael Disaro <michael.disaro@gmail.com>
2023-01-25 10:04:37 +0100

    purged danger

--- Dangerfile.ts
+++
@@ -1,6 +0,0 @@
-// import custom DangerJS rules
-// see http://danger.systems/js
-// see https://github.com/teamdigitale/danger-plugin-digitalcitizenship/
-import checkDangers from 'danger-plugin-digitalcitizenship';
-
-checkDangers();
