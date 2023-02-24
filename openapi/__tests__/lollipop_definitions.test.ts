8448dd44d9314de5214da18c5ac5fdf9f3cde350
AleDore <alessio85.dore@gmail.com>
2023-02-24 13:06:15 +0100

    Add AssertionFileName common definition

--- openapi/__tests__/lollipop_definitions.test.ts
+++ openapi/__tests__/lollipop_definitions.test.ts
@@ -1,33 +1,34 @@
 import { AssertionRef } from "../../generated/definitions/lollipop/AssertionRef";
 import { AssertionType } from "../../generated/definitions/lollipop/AssertionType";
+import { AssertionFileName } from "../../generated/definitions/lollipop/AssertionFileName";
 
 import * as E from "fp-ts/Either";
+import { aFiscalCode } from "../../__mocks__/mocks";
+
+const aSha256AssertionRef =
+  "sha256-p1NY7sl1d4lGvcTyYS535aZR_iJCleEIHFRE2lCHt-c";
+const aSha384AssertionRef =
+  "sha384-pMtZXFXooOW9T0xcE5ahZzpBoIRiwKW2A_OPbVsrTg60f_PyAUIeV0iZUyP19FS-";
+const aSha512AssertionRef =
+  "sha512-qL3u8hwNQp3HuSWdRQr6LFasiedDrUx-fL1n88zH60kj7iUeCpbT4aQ9cZJ3Qkjm4tSuFpBDhhGxqUd5OPQkUA";
 
 describe("AssertionRef - decode", () => {
   it("should decode a valid sha256 assertionRef", () => {
-    const val = "sha256-p1NY7sl1d4lGvcTyYS535aZR_iJCleEIHFRE2lCHt-c";
+    const res = AssertionRef.decode(aSha256AssertionRef);
 
-    const res = AssertionRef.decode(val);
-
-    expect(res).toMatchObject(E.right(val));
+    expect(res).toMatchObject(E.right(aSha256AssertionRef));
   });
 
   it("should decode a valid sha384 assertionRef", () => {
-    const val =
-      "sha384-pMtZXFXooOW9T0xcE5ahZzpBoIRiwKW2A_OPbVsrTg60f_PyAUIeV0iZUyP19FS-";
+    const res = AssertionRef.decode(aSha384AssertionRef);
 
-    const res = AssertionRef.decode(val);
-
-    expect(res).toMatchObject(E.right(val));
+    expect(res).toMatchObject(E.right(aSha384AssertionRef));
   });
 
   it("should decode a valid sha512 assertionRef", () => {
-    const val =
-      "sha512-qL3u8hwNQp3HuSWdRQr6LFasiedDrUx-fL1n88zH60kj7iUeCpbT4aQ9cZJ3Qkjm4tSuFpBDhhGxqUd5OPQkUA";
+    const res = AssertionRef.decode(aSha512AssertionRef);
 
-    const res = AssertionRef.decode(val);
-
-    expect(res).toMatchObject(E.right(val));
+    expect(res).toMatchObject(E.right(aSha512AssertionRef));
   });
 
   it("should not decode a valid assertionRef", () => {
@@ -41,25 +42,17 @@ describe("AssertionRef - decode", () => {
 
 describe("AssertionRef - is", () => {
   it("should check a valid sha256 assertionRef", () => {
-    const val = "sha256-p1NY7sl1d4lGvcTyYS535aZR_iJCleEIHFRE2lCHt-c";
-
-    const res = AssertionRef.is(val);
+    const res = AssertionRef.is(aSha256AssertionRef);
     expect(res).toEqual(true);
   });
 
   it("should check a valid sha384 assertionRef", () => {
-    const val =
-      "sha384-pMtZXFXooOW9T0xcE5ahZzpBoIRiwKW2A_OPbVsrTg60f_PyAUIeV0iZUyP19FS-";
-
-    const res = AssertionRef.is(val);
+    const res = AssertionRef.is(aSha384AssertionRef);
     expect(res).toEqual(true);
   });
 
   it("should decode a valid sha512 assertionRef", () => {
-    const val =
-      "sha512-qL3u8hwNQp3HuSWdRQr6LFasiedDrUx-fL1n88zH60kj7iUeCpbT4aQ9cZJ3Qkjm4tSuFpBDhhGxqUd5OPQkUA";
-
-    const res = AssertionRef.is(val);
+    const res = AssertionRef.is(aSha512AssertionRef);
     expect(res).toEqual(true);
   });
 
@@ -101,5 +94,65 @@ describe("AssertionType - is", () => {
 
     const res = AssertionType.is(val);
     expect(res).toEqual(false);
+  });
+});
+
+describe("AssertionFileName - decode", () => {
+  it("should decode a valid AssertionFileName with sha256 AssertionRef", () => {
+    const val = `${aFiscalCode}-${aSha256AssertionRef}`;
+
+    const res = AssertionFileName.decode(val);
+    expect(res).toMatchObject(E.right(val));
+  });
+
+  it("should decode a valid AssertionFileName with sha384 AssertionRef", () => {
+    const val = `${aFiscalCode}-${aSha384AssertionRef}`;
+
+    const res = AssertionFileName.decode(val);
+    expect(res).toMatchObject(E.right(val));
+  });
+
+  it("should decode a valid AssertionFileName with sha512 AssertionRef", () => {
+    const val = `${aFiscalCode}-${aSha512AssertionRef}`;
+
+    const res = AssertionFileName.decode(val);
+    expect(res).toMatchObject(E.right(val));
+  });
+
+  it("should not decode an invalid assertionRef", () => {
+    const val = "INVALID";
+
+    const res = AssertionType.decode(val);
+    expect(res).toMatchObject(E.left(expect.any(Array)));
+  });
+});
+
+describe("AssertionFileName - is", () => {
+  it("should check a valid AssertionFileName with sha256 AssertionRef", () => {
+    const val = `${aFiscalCode}-${aSha256AssertionRef}`;
+
+    const res = AssertionFileName.is(val);
+    expect(res).toBeTruthy();
+  });
+
+  it("should check a valid AssertionFileName with sha384 AssertionRef", () => {
+    const val = `${aFiscalCode}-${aSha384AssertionRef}`;
+
+    const res = AssertionFileName.is(val);
+    expect(res).toBeTruthy();
+  });
+
+  it("should check a valid AssertionFileName with sha512 AssertionRef", () => {
+    const val = `${aFiscalCode}-${aSha512AssertionRef}`;
+
+    const res = AssertionFileName.is(val);
+    expect(res).toBeTruthy();
+  });
+
+  it("should not check an invalid assertionRef", () => {
+    const val = "INVALID";
+
+    const res = AssertionType.is(val);
+    expect(res).toBeFalsy();
   });
 });
