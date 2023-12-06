2c793e07323ce457d3010a03c496c0e21d61cb1c
Luca Cavallaro <luca.cavallaro@posteo.net>
2023-12-06 18:11:50 +0100

    [IOPID-1024] feat(uee): add ProfileEmail model and isEmailAread

---
+++ src/utils/unique_email_enforcement/index.ts
@@ -0,0 +1,38 @@
+import { EmailString, FiscalCode } from "@pagopa/ts-commons/lib/strings";
+
+import * as t from "io-ts";
+
+export const ProfileEmail = t.type({
+  email: EmailString,
+  fiscalCode: FiscalCode
+});
+
+export type ProfileEmail = t.TypeOf<typeof ProfileEmail>;
+
+export interface IProfileEmailReader {
+  readonly list: (
+    filter: EmailString | FiscalCode
+  ) => AsyncIterableIterator<ProfileEmail>;
+}
+
+export interface IProfileEmailWriter {
+  readonly delete: (p: ProfileEmail) => Promise<void>;
+  readonly insert: (p: ProfileEmail) => Promise<void>;
+}
+
+// Checks if the given e-mail is already taken
+// profileEmails returns all the ProfileEmail records that shares
+// the same e-mail. If count(records) >= 1 then the e-mail is already taken.
+export const isEmailAlreadyTaken = (email: EmailString) => async ({
+  profileEmails
+}: {
+  readonly profileEmails: IProfileEmailReader;
+}): Promise<boolean> => {
+  const emails = profileEmails.list(email);
+  try {
+    const item = await emails.next();
+    return item.done === false;
+  } catch (cause) {
+    throw new Error("unable to obtain taken emails from storage");
+  }
+};
---
+++ src/utils/unique_email_enforcement/storage.ts
@@ -0,0 +1,93 @@
+import * as E from "fp-ts/lib/Either";
+import * as t from "io-ts";
+import { flow } from "fp-ts/lib/function";
+
+import { TableClient, odata } from "@azure/data-tables";
+import { EmailString, FiscalCode } from "@pagopa/ts-commons/lib/strings";
+
+import {
+  ProfileEmail,
+  IProfileEmailReader,
+  IProfileEmailWriter
+} from "./index";
+
+const TableEntity = t.type({
+  partitionKey: t.string,
+  rowKey: t.string
+});
+
+type TableEntity = t.TypeOf<typeof TableEntity>;
+
+const ProfileEmailToTableEntity = new t.Type<ProfileEmail, TableEntity>(
+  "TableEntityFromProfileEmail",
+  ProfileEmail.is,
+  flow(
+    TableEntity.decode,
+    E.chain(({ partitionKey: email, rowKey: fiscalCode }) =>
+      ProfileEmail.decode({ email, fiscalCode })
+    )
+  ),
+  ({ email, fiscalCode }) => ({
+    partitionKey: email.toLowerCase(),
+    rowKey: fiscalCode
+  })
+);
+
+export class DataTableProfileEmailsRepository
+  implements IProfileEmailReader, IProfileEmailWriter {
+  constructor(private readonly tableClient: TableClient) {}
+
+  // Generates an AsyncIterable<ProfileEmail>
+  public async *list(
+    filter: EmailString | FiscalCode
+  ): AsyncIterableIterator<ProfileEmail> {
+    const queryOptions = {
+      filter: EmailString.is(filter)
+        ? odata`PartitionKey eq ${filter.toLowerCase()}`
+        : odata`RowKey eq ${filter}`
+    };
+    const list = this.tableClient.listEntities({
+      queryOptions
+    });
+    try {
+      for await (const item of list) {
+        const profileEmail = ProfileEmailToTableEntity.decode(item);
+        if (E.isLeft(profileEmail)) {
+          throw new Error(`can't parse a profile email from the given entity`, {
+            cause: "parsing"
+          });
+        }
+        yield profileEmail.right;
+      }
+    } catch (e) {
+      if (e instanceof Error && e.cause === "parsing") {
+        throw e;
+      }
+      throw new Error(
+        `unable to get entities from ${this.tableClient.tableName} table`
+      );
+    }
+  }
+
+  public async insert(p: ProfileEmail): Promise<void> {
+    try {
+      const entity = ProfileEmailToTableEntity.encode(p);
+      await this.tableClient.createEntity(entity);
+    } catch {
+      throw new Error(
+        `unable to insert a new profile entity into ${this.tableClient.tableName} table`
+      );
+    }
+  }
+
+  public async delete(p: ProfileEmail): Promise<void> {
+    try {
+      const entity = ProfileEmailToTableEntity.encode(p);
+      await this.tableClient.deleteEntity(entity.partitionKey, entity.rowKey);
+    } catch {
+      throw new Error(
+        `unable to delete the specified entity from ${this.tableClient.tableName} table`
+      );
+    }
+  }
+}
