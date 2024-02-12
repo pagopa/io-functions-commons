8ab0a6336e98ee9d6f82eeaaa14bfcd7f17e0b45
garma00 <giovanniarmaleo00@gmail.com>
2024-02-12 16:55:01 +0100

    Fix mappers

--- src/utils/rc_configuration.ts
+++ src/utils/rc_configuration.ts
@@ -1,8 +1,8 @@
 import { withoutUndefinedValues } from "@pagopa/ts-commons/lib/types";
+import { RCConfigurationPublic } from "../../generated/definitions/RCConfigurationPublic";
 import { RCAuthenticationConfig } from "../../generated/definitions/RCAuthenticationConfig";
 import { RCClientCert } from "../../generated/definitions/RCClientCert";
 import { RCConfigurationProdEnvironment } from "../../generated/definitions/RCConfigurationProdEnvironment";
-import { RCConfigurationResponse } from "../../generated/definitions/RCConfigurationResponse";
 import { RCConfigurationTestEnvironment } from "../../generated/definitions/RCConfigurationTestEnvironment";
 import {
   RCTestEnvironmentConfig,
@@ -41,28 +41,28 @@ const getProdEnvironment = (
   prodEnvironment: RCEnvironmentConfig
 ): RCConfigurationProdEnvironment => ({
   base_url: prodEnvironment.baseUrl,
-  details_authentication: prodEnvironment.detailsAuthentication
-    ? getDetailsAuthentication(prodEnvironment.detailsAuthentication)
-    : undefined
+  details_authentication: getDetailsAuthentication(
+    prodEnvironment.detailsAuthentication
+  )
 });
 
 /**
  * Converts a RetrievedRCConfiguration to a configuration that can be shared via API
  */
 export const retrievedRCConfigurationToPublic = (
-  retrievedConfiguration: RetrievedRCConfiguration
-): RCConfigurationResponse =>
+  publicConfiguration: RetrievedRCConfiguration
+): RCConfigurationPublic =>
   withoutUndefinedValues({
-    configuration_id: retrievedConfiguration.configurationId,
-    description: retrievedConfiguration.description,
-    disable_lollipop_for: retrievedConfiguration.disableLollipopFor,
-    has_precondition: retrievedConfiguration.hasPrecondition,
-    is_lollipop_enabled: retrievedConfiguration.isLollipopEnabled,
-    name: retrievedConfiguration.name,
-    prod_environment: retrievedConfiguration.prodEnvironment
-      ? getProdEnvironment(retrievedConfiguration.prodEnvironment)
+    configuration_id: publicConfiguration.configurationId,
+    description: publicConfiguration.description,
+    disable_lollipop_for: publicConfiguration.disableLollipopFor,
+    has_precondition: publicConfiguration.hasPrecondition,
+    is_lollipop_enabled: publicConfiguration.isLollipopEnabled,
+    name: publicConfiguration.name,
+    prod_environment: publicConfiguration.prodEnvironment
+      ? getProdEnvironment(publicConfiguration.prodEnvironment)
       : undefined,
-    test_environment: retrievedConfiguration.testEnvironment
-      ? getTestEnvironment(retrievedConfiguration.testEnvironment)
+    test_environment: publicConfiguration.testEnvironment
+      ? getTestEnvironment(publicConfiguration.testEnvironment)
       : undefined
   });
