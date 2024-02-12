e7e5be9cff560ff9425c38c0e6981b06fa4d7b33
Garma <64153597+Garma00@users.noreply.github.com>
2024-02-12 10:13:47 +0100

    [IOCOM-1066] Fix openapi for RC (#364)

--- src/models/rc_configuration.ts
+++ src/models/rc_configuration.ts
@@ -1,21 +1,89 @@
 import * as t from "io-ts";
 
+import {
+  FiscalCode,
+  NonEmptyString,
+  Ulid
+} from "@pagopa/ts-commons/lib/strings";
+import { enumType } from "@pagopa/ts-commons/lib/types";
 import { Container } from "@azure/cosmos";
 import {
   CosmosdbModelVersioned,
   RetrievedVersionedModel
 } from "../utils/cosmosdb_model_versioned";
-import { RCConfigurationBase } from "../../generated/definitions/RCConfigurationBase";
-import { RCConfigurationEnvironment } from "../../generated/definitions/RCConfigurationEnvironment";
+import { HasPreconditionEnum } from "../../generated/definitions/HasPrecondition";
 
 export const RC_CONFIGURATION_COLLECTION_NAME = "remote-content-configuration";
 
 const RC_CONFIGURATION_MODEL_PK_FIELD = "configurationId";
 
+export const RCClientCert = t.interface({
+  clientCert: NonEmptyString,
+  clientKey: NonEmptyString,
+  serverCa: NonEmptyString
+});
+export type RCClientCert = t.TypeOf<typeof RCClientCert>;
+
+const RCAuthenticationDetails = t.interface({
+  headerKeyName: NonEmptyString,
+  key: NonEmptyString,
+  type: NonEmptyString
+});
+
+export type RCAuthenticationConfig = t.TypeOf<typeof RCAuthenticationConfig>;
+export const RCAuthenticationConfig = t.intersection([
+  RCAuthenticationDetails,
+  t.partial({ cert: RCClientCert })
+]);
+
+export type RCEnvironmentConfig = t.TypeOf<typeof RCEnvironmentConfig>;
+export const RCEnvironmentConfig = t.interface({
+  baseUrl: NonEmptyString,
+  detailsAuthentication: RCAuthenticationConfig
+});
+
+export type RCTestEnvironmentConfig = t.TypeOf<typeof RCTestEnvironmentConfig>;
+export const RCTestEnvironmentConfig = t.intersection([
+  t.interface({
+    testUsers: t.readonlyArray(FiscalCode)
+  }),
+  RCEnvironmentConfig
+]);
+
+const RCConfigurationR = t.interface({
+  configurationId: Ulid,
+  description: NonEmptyString,
+  disableLollipopFor: t.readonlyArray(FiscalCode),
+  hasPrecondition: enumType<HasPreconditionEnum>(
+    HasPreconditionEnum,
+    "hasPrecondition"
+  ),
+  isLollipopEnabled: t.boolean,
+  name: NonEmptyString,
+  userId: NonEmptyString
+});
+
+const RCConfigurationO = t.partial({});
+
+export const RCConfigurationBase = t.intersection([
+  RCConfigurationR,
+  RCConfigurationO
+]);
+export type RCConfigurationBase = t.TypeOf<typeof RCConfigurationBase>;
+
 export type RCConfiguration = t.TypeOf<typeof RCConfiguration>;
 export const RCConfiguration = t.intersection([
   RCConfigurationBase,
-  RCConfigurationEnvironment
+  t.union([
+    t.intersection([
+      t.interface({ prodEnvironment: RCEnvironmentConfig }),
+      t.partial({ testEnvironment: RCTestEnvironmentConfig })
+    ]),
+    t.intersection([
+      t.partial({ prodEnvironment: RCEnvironmentConfig }),
+      t.interface({ testEnvironment: RCTestEnvironmentConfig })
+    ])
+  ])
 ]);
 
 export const RetrievedRCConfiguration = t.intersection([
---
+++ src/utils/rc_configuration.ts
@@ -0,0 +1,68 @@
+import { withoutUndefinedValues } from "@pagopa/ts-commons/lib/types";
+import { RCAuthenticationConfig } from "../../generated/definitions/RCAuthenticationConfig";
+import { RCClientCert } from "../../generated/definitions/RCClientCert";
+import { RCConfigurationProdEnvironment } from "../../generated/definitions/RCConfigurationProdEnvironment";
+import { RCConfigurationResponse } from "../../generated/definitions/RCConfigurationResponse";
+import { RCConfigurationTestEnvironment } from "../../generated/definitions/RCConfigurationTestEnvironment";
+import {
+  RCTestEnvironmentConfig,
+  RetrievedRCConfiguration,
+  RCAuthenticationConfig as RCAuthenticationConfigModel,
+  RCClientCert as RCClientCertModel,
+  RCEnvironmentConfig
+} from "../models/rc_configuration";
+
+const getCert = (cert: RCClientCertModel): RCClientCert => ({
+  client_cert: cert.clientCert,
+  client_key: cert.clientKey,
+  server_ca: cert.serverCa
+});
+
+const getDetailsAuthentication = (
+  detailsAuth: RCAuthenticationConfigModel
+): RCAuthenticationConfig => ({
+  cert: detailsAuth.cert ? getCert(detailsAuth.cert) : undefined,
+  header_key_name: detailsAuth.headerKeyName,
+  key: detailsAuth.key,
+  type: detailsAuth.type
+});
+
+const getTestEnvironment = (
+  testEnv: RCTestEnvironmentConfig
+): RCConfigurationTestEnvironment => ({
+  base_url: testEnv.baseUrl,
+  details_authentication: getDetailsAuthentication(
+    testEnv.detailsAuthentication
+  ),
+  test_users: testEnv.testUsers
+});
+
+const getProdEnvironment = (
+  prodEnvironment: RCEnvironmentConfig
+): RCConfigurationProdEnvironment => ({
+  base_url: prodEnvironment.baseUrl,
+  details_authentication: prodEnvironment.detailsAuthentication
+    ? getDetailsAuthentication(prodEnvironment.detailsAuthentication)
+    : undefined
+});
+
+/**
+ * Converts a RetrievedRCConfiguration to a configuration that can be shared via API
+ */
+export const retrievedRCConfigurationToPublic = (
+  retrievedConfiguration: RetrievedRCConfiguration
+): RCConfigurationResponse =>
+  withoutUndefinedValues({
+    configuration_id: retrievedConfiguration.configurationId,
+    description: retrievedConfiguration.description,
+    disable_lollipop_for: retrievedConfiguration.disableLollipopFor,
+    has_precondition: retrievedConfiguration.hasPrecondition,
+    is_lollipop_enabled: retrievedConfiguration.isLollipopEnabled,
+    name: retrievedConfiguration.name,
+    prod_environment: retrievedConfiguration.prodEnvironment
+      ? getProdEnvironment(retrievedConfiguration.prodEnvironment)
+      : undefined,
+    test_environment: retrievedConfiguration.testEnvironment
+      ? getTestEnvironment(retrievedConfiguration.testEnvironment)
+      : undefined
+  });
