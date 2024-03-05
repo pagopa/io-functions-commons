a1018af34de67e35a83fa6072f81e7652e6920b7
Garma <64153597+Garma00@users.noreply.github.com>
2024-03-05 17:08:39 +0100

    [#IOCOM-1160] Add findAll for RC (#369)

--- src/models/user_rc_configuration.ts
+++ src/models/user_rc_configuration.ts
@@ -1,10 +1,20 @@
 import * as t from "io-ts";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as RA from "fp-ts/lib/ReadonlyArray";
+
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { Container } from "@azure/cosmos";
-import { AzureCosmosResource, CosmosdbModel } from "../utils/cosmosdb_model";
+import { pipe } from "fp-ts/lib/function";
+import {
+  AzureCosmosResource,
+  CosmosdbModel,
+  CosmosErrors,
+  toCosmosErrorResponse
+} from "../utils/cosmosdb_model";
+import { asyncIterableToArray } from "../utils/async";
 
 export const USER_RC_CONFIGURATIONS_COLLECTION_NAME = "user-configurations";
-const USER_RC_CONFIGURATIONS_MODEL_PK_FIELD = "userId";
+export const USER_RC_CONFIGURATIONS_MODEL_PK_FIELD = "userId";
 
 export const UserRCConfiguration = t.interface({
   id: NonEmptyString,
@@ -33,5 +43,32 @@ export class UserRCConfigurationModel extends CosmosdbModel<
    */
   constructor(container: Container) {
     super(container, UserRCConfiguration, RetrievedUserRCConfiguration);
+  }
+
+  /**
+   * Returns all the UserRCConfiguration associated to the userId
+   *
+   * @param userId The userId of the caller
+   */
+  public findAllByUserId(
+    userId: NonEmptyString
+  ): TE.TaskEither<CosmosErrors, ReadonlyArray<RetrievedUserRCConfiguration>> {
+    const querySpec = {
+      parameters: [
+        {
+          name: "@userId",
+          value: userId
+        }
+      ],
+      query: `SELECT * FROM n WHERE n.userId = @userId`
+    };
+    return pipe(
+      TE.tryCatch(
+        () => asyncIterableToArray(this.getQueryIterator(querySpec)),
+        toCosmosErrorResponse
+      ),
+      TE.map(RA.flatten),
+      TE.map(RA.rights)
+    );
   }
 }
