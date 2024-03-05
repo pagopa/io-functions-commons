364c351b91f67129b4717c0f912f0f133f01b4b9
garma00 <giovanniarmaleo00@gmail.com>
2024-03-05 11:21:53 +0100

    Add findAll for RC

--- src/models/rc_configuration.ts
+++ src/models/rc_configuration.ts
@@ -1,4 +1,6 @@
 import * as t from "io-ts";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as RA from "fp-ts/lib/ReadonlyArray";
 
 import {
   FiscalCode,
@@ -7,15 +9,18 @@ import {
 } from "@pagopa/ts-commons/lib/strings";
 import { enumType } from "@pagopa/ts-commons/lib/types";
 import { Container } from "@azure/cosmos";
+import { pipe } from "fp-ts/lib/function";
 import {
   CosmosdbModelVersioned,
   RetrievedVersionedModel
 } from "../utils/cosmosdb_model_versioned";
 import { HasPreconditionEnum } from "../../generated/definitions/HasPrecondition";
+import { CosmosErrors, toCosmosErrorResponse } from "../utils/cosmosdb_model";
+import { asyncIterableToArray } from "../utils/async";
 
 export const RC_CONFIGURATION_COLLECTION_NAME = "message-configuration";
 
-const RC_CONFIGURATION_MODEL_PK_FIELD = "configurationId";
+export const RC_CONFIGURATION_MODEL_PK_FIELD = "configurationId";
 
 export const RCClientCert = t.interface({
   clientCert: NonEmptyString,
@@ -98,6 +103,56 @@ export class RCConfigurationModel extends CosmosdbModelVersioned<
       RCConfiguration,
       RetrievedRCConfiguration,
       RC_CONFIGURATION_MODEL_PK_FIELD
+    );
+  }
+
+  /**
+   * Returns all the RCConfiguration identified by every configurationId
+   *
+   * @param configurationIds an array of configurationId
+   */
+  public findAllLastVersionByConfigurationId(
+    configurationIds: ReadonlyArray<Ulid>
+  ): TE.TaskEither<CosmosErrors, ReadonlyArray<RetrievedRCConfiguration>> {
+    if (configurationIds.length === 0) {
+      return TE.left(
+        toCosmosErrorResponse(new Error("Empty configurationId array"))
+      );
+    }
+    const querySpec = {
+      parameters: [
+        {
+          name: "@configurationIds",
+          value: configurationIds
+        }
+      ],
+      query: `SELECT * FROM n WHERE ARRAY_CONTAINS(@configurationIds, n.${RC_CONFIGURATION_MODEL_PK_FIELD})`
+    };
+    return pipe(
+      TE.tryCatch(
+        () => asyncIterableToArray(this.getQueryIterator(querySpec)),
+        toCosmosErrorResponse
+      ),
+      TE.map(RA.flatten),
+      TE.map(RA.rights),
+      TE.map(retrievedRCConfigurations =>
+        this.filterLastVersion(retrievedRCConfigurations, configurationIds)
+      )
+    );
+  }
+
+  /**
+   * Filter the last version of every configuration identified by every configurationId
+   * */
+  private filterLastVersion(
+    retrievedRCConfiguration: ReadonlyArray<RetrievedRCConfiguration>,
+    configurationIds: ReadonlyArray<Ulid>
+  ): ReadonlyArray<RetrievedRCConfiguration> {
+    return configurationIds.map(
+      configurationId =>
+        retrievedRCConfiguration
+          .filter(rc => rc.configurationId === configurationId)
+          .sort((a, b) => b.version - a.version)[0]
     );
   }
 }
