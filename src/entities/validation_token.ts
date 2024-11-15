4c33ce14f25cacd25ca6ac92722238aa3ff63067
Salvatore Laiso <salvatore.laiso@it.ey.com>
2024-11-15 15:59:48 +0100

    fix: upgraded `ValidationTokenEntity`

--- src/entities/validation_token.ts
+++ src/entities/validation_token.ts
@@ -7,7 +7,6 @@ import * as t from "io-ts";
 import { UTCISODateFromString } from "@pagopa/ts-commons/lib/dates";
 import { EmailAddress } from "../../generated/definitions/EmailAddress";
 import { FiscalCode } from "../../generated/definitions/FiscalCode";
-import { TableEntity } from "../utils/azure_storage";
 import { TableEntityAzureDataTables } from "../utils/data_tables";
 
 export const VALIDATION_TOKEN_TABLE_NAME = "ValidationTokens";
@@ -22,24 +21,9 @@ export const ValidationToken = t.interface({
   InvalidAfter: UTCISODateFromString
 });
 
-export const ValidationTokenEntityAzureDataTables = t.intersection([
+export const ValidationTokenEntity = t.intersection([
   ValidationToken,
   TableEntityAzureDataTables
 ]);
 
-export const ValidationTokenEntity = t.intersection([
-  ValidationToken,
-  TableEntity
-]);
-
-/**
- * Type to be used with `@azure/data-tables`
- */
-export type ValidationTokenEntityAzureDataTables = t.TypeOf<
-  typeof ValidationTokenEntityAzureDataTables
->;
-
-/**
- * Type to be used with the deprecated `azure-storage`
- */
 export type ValidationTokenEntity = t.TypeOf<typeof ValidationTokenEntity>;
