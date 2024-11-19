73d9235c7ac6bac04045d8441cd56e7b32efbb41
Salvatore Laiso <salvatore.laiso@it.ey.com>
2024-11-19 09:38:58 +0100

    fix: import

--- src/utils/unique_email_enforcement/storage.ts
+++ src/utils/unique_email_enforcement/storage.ts
@@ -5,7 +5,7 @@ import { flow } from "fp-ts/lib/function";
 import { TableClient, odata, RestError } from "@azure/data-tables";
 import { EmailString } from "@pagopa/ts-commons/lib/strings";
 
-import { TableEntity } from "../data_tables";
+import { TableEntityAzureDataTables } from "../data_tables";
 
 import {
   ProfileEmail,
@@ -14,11 +14,14 @@ import {
   ProfileEmailWriterError
 } from "./index";
 
-const ProfileEmailToTableEntity = new t.Type<ProfileEmail, TableEntity>(
+const ProfileEmailToTableEntity = new t.Type<
+  ProfileEmail,
+  TableEntityAzureDataTables
+>(
   "TableEntityFromProfileEmail",
   ProfileEmail.is,
   flow(
-    TableEntity.decode,
+    TableEntityAzureDataTables.decode,
     E.chain(({ partitionKey: email, rowKey: fiscalCode }) =>
       ProfileEmail.decode({ email, fiscalCode })
     )
