21bc33af57da7ab953d6817ebc8076691889cec3
gquadrati <greta.quadrati@pagopa.it>
2026-02-11 13:33:31 +0100

    upgrade to node 20 and to azure/functions v4

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,28 +1,29 @@
-import { Context } from "@azure/functions";
+import { InvocationContext } from "@azure/functions";
 import { LogEntry } from "winston";
 import * as Transport from "winston-transport";
 
 /**
- * Returns the Context logging function matching the provided logging level
+ * Returns the InvocationContext logging function matching the provided logging level
+ * In Azure Functions v4, context.log is a simple function without sub-methods
  */
 const getLoggerForLevel = (
-  logger: Context["log"],
+  context: InvocationContext,
   level: string
   // eslint-disable-next-line @typescript-eslint/array-type
 ): ((...args: readonly unknown[]) => void) => {
   switch (level) {
     case "debug":
-      return logger.verbose;
+      return context.debug;
     case "info":
-      return logger.info;
+      return context.info;
     case "warn":
-      return logger.warn;
+      return context.warn;
     case "error":
-      return logger.error;
+      return context.error;
     default:
       // eslint-disable-next-line @typescript-eslint/array-type
       return (...args: readonly unknown[]): void =>
-        logger.info(`[${level}] `, ...args);
+        context.info(`[${level}] `, ...args);
   }
 };
 
@@ -31,11 +32,11 @@ const getLoggerForLevel = (
  */
 export class AzureContextTransport extends Transport {
   /**
-   * @param getContextLogger A function that returns the `log` method in the Context
+   * @param getContextLogger A function that returns the `log` method in the InvocationContext
    * @param options Extra transport options
    */
   constructor(
-    private readonly getContextLogger: () => Context["log"] | undefined,
+    private readonly getContextLogger: () => InvocationContext | undefined,
     options: Transport.TransportStreamOptions
   ) {
     super(options);
