44dee20c074b3aca9fa69bab408c34d2cbab0a28
gquadrati <greta.quadrati@pagopa.it>
2026-02-13 15:37:55 +0100

    remove legacy jest config, extended jest timeout

--- __integrations__/jest.config.js
+++ __integrations__/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: "ts-jest",
   collectCoverage: true,
   testEnvironment: "node",
-  setupFilesAfterFramework: [],
+  testTimeout: 10000,
   setupFiles: ["./jest.setup.js"],
   testPathIgnorePatterns: ["dist", "/node_modules"]
 };
